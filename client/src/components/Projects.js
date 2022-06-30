9b0b168f2e9a48155af84297fe1a8799fd9b0c27
Adrian Reyes <adrian_reyes1@yahoo.com>
2022-06-29 18:21:29 -0700

    fixed sizing issue with projects section

--- client/src/components/Projects.js
+++ client/src/components/Projects.js
@@ -2,10 +2,11 @@ import { projectList } from "./ProjectData";
 
 function Projects() {
   let projectsMap = projectList.map(project => {
+    /* d-flex flex-row justify-content-around align-items-center my-1 border border-start-0 rounded-end */
     return (
       <div
         key={project.name}
-        className="project-list d-flex flex-row justify-content-around align-items-center my-1 border border-start-0 rounded-end"
+        className="project-list d-flex flex-row justify-content-around align-items-center my-1 border border-start-0 rounded-end flex-wrap"
       >
         <div className="w-25 px-2">
           <h1>{project.name}</h1>
@@ -15,7 +16,11 @@ function Projects() {
           </div>
         </div>
         <p className="w-50 px-2">{project.description}</p>
-        <img src={project.img} alt="logo" className="h-75 rounded" />
+        <img
+          src={project.img}
+          alt="project-image"
+          className="project-image rounded-3 p-4"
+        />
       </div>
     );
   });
@@ -23,7 +28,9 @@ function Projects() {
   return (
     <div>
       <h1>Projects</h1>
-      <div>{projectsMap}</div>
+      <div className="d-flex justify-content-around flex-wrap">
+        {projectsMap}
+      </div>
     </div>
   );
 }
